7b9c27e30ce9f10c96f9dc747f517f0c4d07fbf8
ManavPandya31 <manavpandya42@gmail.com>
2025-03-07 15:09:14 +0530

    Add Files

---
+++ ConditionalStatements&loops/Marksheet.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int marks;
+
+    cout <<  "Enter A Marks :- ";
+    cin >> marks;
+
+    if (marks >= 90){
+
+        cout << "Grade A";
+
+    } else if (marks >= 80 && marks < 90) {
+
+        cout << "Grade B";
+        
+    }else if (marks >= 70 && marks < 80){
+
+        cout << "Grade C";
+
+    }else if (marks >= 50 && marks < 70){
+
+        cout << "Grade D";
+
+    }else if (marks >= 35 && marks < 50){
+
+        cout << "Grade E";
+
+    }else{
+
+        cout << "Fail";
+    }
+
+}
---
+++ ConditionalStatements&loops/UppercaseLowercase.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    char ch;
+
+    cout << "Enter A Any Chacter :- ";
+    cin >> ch;
+
+   // if (ch >= 'a' && ch <= 'z'){
+   if (ch >= 97 && ch <= 122){  // Both If Condition Is True
+
+        cout << "Lowercase\n";
+
+    }else{
+
+        cout << "Uppercase ";
+
+    }
+}
