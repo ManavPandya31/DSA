1fb34cdb6f553c0ffbdc135286a9b1a6dabe5d9f
ManavPandya31 <manavpandya42@gmail.com>
2025-03-07 14:44:45 +0530

    Odd Even

---
+++ ConditionalStatements&loops/OddEven.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int number;
+    cout << "Enter A Number :- ";
+    cin >> number;
+
+    if (number % 2 == 0){
+
+        cout << "Number Is Even..";
+
+    }
+    else{
+
+        cout << "Number Is Odd.";
+    }
+}
