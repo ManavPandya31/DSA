0959e43adcf28346c50e1b14c652629433caa3b6
ManavPandya31 <manavpandya42@gmail.com>
2025-03-07 14:15:42 +0530

    if-else

---
+++ ConditionalStatements&loops/if-else.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int age;
+    cout << "Enter A Age :- ";
+    cin >> age;
+
+    if (age >= 18){
+
+        cout << "You Are Eligable For Voting..";
+
+    }
+    else{
+        cout << "You Are Not Eligable..";
+    }
+}
